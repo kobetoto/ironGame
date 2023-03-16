c9fa36e0d88d68c64c3f3d7b1c0f86fe07986c93
kobetoto <106164290+kobetoto@users.noreply.github.com>
2023-03-16 14:25:05 +0100

    iteration3

--- IronGame/IronGme.js
+++ IronGame/IronGme.js
@@ -13,11 +13,33 @@ const myGameArea = {
     const canvas = this.canvas;//<canvas>
     canvas.width = 1200;
     canvas.height = 1000;
-
     this.context = canvas.getContext("2d");// dans myGameArea jai le context
-
+    console.log('this.context=', this.context)
     document.body.appendChild(canvas);
+
+    this.bgimage = document.createElement('img'); // <img />
+    this.bgimage.src= 'https://cdn.discordapp.com/attachments/1084937735581741116/1085546667396313098/totodadababa_background_basketball_video_game_pixar_style_55b86151-eefa-4984-ac04-2216a23e1dbb.png'
+    
   },
+  draw: function(){ // toutes les 16ms🎦
+    const ctx = this.context;//console.log('ctx=', ctx) =>DEBUG
+
+    // clear
+    ctx.clearRect(0,0,1200,1000);
+
+
+    ctx.drawImage(this.bgimage,0,0,1200,1000); //draw=>bgimage=>backGround
+
+    ball.draw(100,100);//draw=>ball
+    hoop.draw(210, 375, 42, 9, 'red');
+    board.draw(150,300,118,88,'blue');
+
+
+    this.int = requestAnimationFrame(() => {this.draw()})// rappel toutes les 16ms la Function DRAW
+  },
+  stop: function () {
+    cancelAnimationFrame(this.int)
+  }
 };
 
 const myScoreBoard = {
@@ -41,20 +63,27 @@ const myScoreBoard = {
 ██      ██      ██      ██  ██  ██ ██      ██  ██ ██    ██    
 ███████ ███████ ███████ ██      ██ ███████ ██   ████    ██   
 */
-const player = {
+/*const player = {
 
-}
+  draw: function (x,y,w,h) {
+    const ctx = myGameArea.context;
+    ctx.drawImage();
+
+  }
+}*/
 
 //ball 🏀
 let ball = {
 
   draw: function (x,y) {
+
     const ctx = myGameArea.context;
     ctx.beginPath();
     ctx.arc(x, y, 23, 0, 2 * Math.PI);
     ctx.fillStyle = "orange";
     ctx.fill();
     ctx.closePath();
+
   }
 }
 
@@ -88,6 +117,7 @@ const board = new Hoop(150,300,118,88,'white'); //avec ou sans BORD????
 */
 document.getElementById('start-button').onclick = () => {
   myGameArea.start();
+  myGameArea.draw();
   //myScoreBoard.start();
 };
 
@@ -98,6 +128,5 @@ document.getElementById('start-button').onclick = () => {
 ██   ██ ██  ██ ██ ██ ██  ██  ██ ██   ██    ██    ██ ██    ██ ██  ██ ██     ██      ██    ██ ██    ██ ██      
 ██   ██ ██   ████ ██ ██      ██ ██   ██    ██    ██  ██████  ██   ████     ███████  ██████   ██████  ██   
 */ 
-
 
 //ANIMATION LOOP
